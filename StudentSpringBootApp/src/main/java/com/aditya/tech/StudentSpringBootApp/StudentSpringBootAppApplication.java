6f1b892c03b9e016ac4521b227dc9b2f6b659805
adityasrivastva <aditya5srivastva@gmail.com>
2018-03-03 04:50:39 +0530

    Hello

---
+++ StudentSpringBootApp/src/main/java/com/aditya/tech/StudentSpringBootApp/StudentSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.tech.StudentSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSpringBootAppApplication.class, args);
+		
+	}
+}

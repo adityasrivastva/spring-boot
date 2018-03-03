85ffe4f9273c192ff2d500b4d23ada05fcd36d4d
adityasrivastva <aditya5srivastva@gmail.com>
2018-03-03 21:32:45 +0530

    Added Project for Tkt Booking

---
+++ TickectBookingManagementApp/src/main/java/com/aditya/tech/bookticket/app/TickectBookingManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.aditya.tech.bookticket.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickectBookingManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickectBookingManagementAppApplication.class, args);
+		
+	}
+}
